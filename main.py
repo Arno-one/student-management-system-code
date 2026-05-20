bbd5e0e7ce102b961174dcfbcf40acca3b61c8f6
梦星河 <2839866152@qq.com>
2026-05-20 12:18:47 +0800

    main文件分类

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from fastapi import FastAPI
 import uvicorn
 app = FastAPI()
 
+#学⽣基本信息管理模块
+
+#学⽣考核成绩管理模块
+
+#学⽣就业管理模块
+
+# 班级管理模块
+
+#⽼师管理模块
+
+#统计分析模块
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='localhost', port=8088,)
