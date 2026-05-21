56149146ff9d658b43ca564a9c40175222f58b67
梦星河 <2839866152@qq.com>
2026-05-21 15:33:52 +0800

    学生信息添加main

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ import uvicorn
 from API.statistical import sta_router
 from API.score_api import router_score
 from API.class_api import class_router
+from API.student_api import student_router
 
 app = FastAPI()
 
 # 学⽣基本信息管理模块
-
+app.include_router(student_router, prefix='/student', tags=['学⽣基本信息管理'])
 # 学⽣考核成绩管理模块
 app.include_router(router_score, prefix='/score', tags=['学⽣考核成绩管理'])
 # 学⽣就业管理模块
 
 # 班级管理模块
-app.include_router(class_router,prefix="",tags=["班级管理"])
+app.class_router(class_router,prefix="/class",tags=["班级管理"])
 # ⽼师管理模块
 
 # 统计分析模块
